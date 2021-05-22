07d85a676e9703e4a9418cc25b8d95b53fe3590c
unknown <nyangedickson@gmail.com>
2021-05-22 15:07:25 +0300

    Finalised app - pomodoro app

--- pomodoro-timer/functions.js
+++ pomodoro-timer/functions.js
@@ -1,22 +1,29 @@
 const renderPomodoro = () => {
     return `<div class="pomodoro">
                     <h1 id="pomodoro-timer"></h1>
-                    <a href="" class="btn-start">Start</a>
+                    <a href="" class="btn-start" id="btn-start">Start</a>
                 </div>`;
 }
 
 const renderShortBreak = () => {
     return `<div class="pomodoro">
                     <h1 id="short-break-timer">10:00</h1>
-                    <a href="" class="btn-start">Start</a>
+                    <a href="" class="btn-start" id="btn-start">Start</a>
                 </div>`;
 }
 
 const renderLongBreak = () => {
     return `<div class="pomodoro">
                     <h1 id="long-break-timer">24:00</h1>
-                    <a href="" class="btn-start">Start</a>
+                    <a href="" class="btn-start" id="btn-start">Start</a>
                 </div>`;
 }
 
-export { renderPomodoro, renderShortBreak, renderLongBreak };
+const audioplay = (audio) => {
+    return `<audio id="MyAudioElement" autoplay>
+            <source src=${audio} type="audio/mpeg">
+            Your browser does not support the audio element.
+        </audio>`
+}
+
+export { renderPomodoro, renderShortBreak, renderLongBreak, audioplay };
--- pomodoro-timer/main.js
+++ pomodoro-timer/main.js
@@ -1,4 +1,4 @@
-import { renderPomodoro, renderShortBreak, renderLongBreak } from './functions.js';
+import { renderPomodoro, renderShortBreak, renderLongBreak, audioplay } from './functions.js';
 import { pomoDoroClasses, shortBreakClasses, longBreakClasses } from './orgClasses.js';
 
 const body = document.getElementById('body');
@@ -8,64 +8,139 @@ const longBreak = document.getElementById('long-break');
 const timerHandler = document.getElementById('timer-handler');
 const launchModal = document.getElementById('launch-modal');
 const modal = document.getElementById('modal-settings-container');
-const date = new Date();
+const ringMeElements = document.getElementById('ring');
+const audioUrl = './ring-me.mp3';
+
+// sound - ring after session
+const ring = () => {
+    return ringMeElements.innerHTML = audioplay(audioUrl);
+}
 
 window.onload = () => {
+
+    localStorage.setItem("pomodoro", 25);
+    localStorage.setItem("shortbreak", 10);
+    localStorage.setItem("longbreak", 15);
+
     timerHandler.innerHTML = renderPomodoro();
     pomodoro.classList.add('active');
-    console.log(timerHandler);
 
-    // const timerPomo = document.getElementById('pomodoro-timer');
-    // timerPomo.innerHTML = "90:00";
+    const timerPomo = document.getElementById('pomodoro-timer');
+    const btnStart = document.getElementById('btn-start');
+    timerPomo.innerHTML = `Welcome`;
 
-    runTimer('pomodoro-timer', '01:00');
+    btnStart.addEventListener('click', (e) => {
+        e.preventDefault();
+        var fiveMinutes = 60 * localStorage.getItem('pomodoro');
+        initTimer(fiveMinutes, timerPomo);
+    })
 
 };
-
-
 
 pomodoro.addEventListener('click', (e) => {
     e.preventDefault();
     timerHandler.innerHTML = renderPomodoro();
     pomodoro.classList.add('active');
     pomoDoroClasses(shortBreak, longBreak, body);
+
     const timerPomo = document.getElementById('pomodoro-timer');
-    timerPomo.innerHTML = "90:00";
+    const btnStart = document.getElementById('btn-start');
+    timerPomo.innerHTML = `${localStorage.getItem('pomodoro')}:00`;
+
+    btnStart.addEventListener('click', (e) => {
+        e.preventDefault();
+        var fiveMinutes = 60 * localStorage.getItem('pomodoro');
+        initTimer(fiveMinutes, timerPomo);
+    })
+
 });
 
 shortBreak.addEventListener('click', (e) => {
     e.preventDefault();
     timerHandler.innerHTML = renderShortBreak();
     shortBreakClasses(shortBreak, pomodoro, longBreak);
+
+    const timerShort = document.getElementById('short-break-timer');
+    const btnStart = document.getElementById('btn-start');
+    timerShort.innerHTML = `${localStorage.getItem('shortbreak')}:00`;
+
+    btnStart.addEventListener('click', (e) => {
+        e.preventDefault();
+        var fiveMinutes = 60 * localStorage.getItem('shortbreak');
+        initTimer(fiveMinutes, timerShort);
+    })
+
 });
 
 longBreak.addEventListener('click', (e) => {
     e.preventDefault();
     timerHandler.innerHTML = renderLongBreak();
     longBreakClasses(shortBreak, pomodoro, longBreak, body);
+
+    const timerLong = document.getElementById('long-break-timer');
+    const btnStart = document.getElementById('btn-start');
+    timerLong.innerHTML = `${localStorage.getItem('longbreak')}:00`;
+
+    btnStart.addEventListener('click', (e) => {
+        e.preventDefault();
+        var fiveMinutes = 60 * localStorage.getItem('longbreak');
+        initTimer(fiveMinutes, timerLong);
+    })
+
 });
 
+const initTimer = (duration, el_id) => {
+    var timer = duration, minutes, seconds;
+    var timing = setInterval(() => {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+
+        el_id.textContent = minutes + ':' + seconds;
+
+        if (--timer < 0) {
+            clearInterval(timing);
+            ring();
+        }
+
+    }, 1000)
+};
+
+// modal inputs
 launchModal.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('...modal');
     modal.classList.add('modal-view');
+
+    var sendBtn = document.getElementById('setButton');
+
+    sendBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        var setPomoTimer = document.getElementById('setPomoTimer').value;
+        var setShortBreak = document.getElementById('setShortBreak').value;
+        var setLongBreak = document.getElementById('setLongBreak').value;
+
+        if (setPomoTimer != '' && setLongBreak != '' && setShortBreak != '') {
+
+            localStorage.clear();
+            localStorage.setItem("pomodoro", setPomoTimer);
+            localStorage.setItem("shortbreak", setShortBreak);
+            localStorage.setItem("longbreak", setLongBreak);
+
+            modal.classList.remove('modal-view');
+
+        } else {
+            localStorage.getItem("pomodoro");
+            localStorage.getItem("shortbreak");
+            localStorage.getItem("longbreak");
+            modal.classList.remove('modal-view');
+        }
+
+    });
+
 });
 
-const timer = (endtimer) => {
-    const countdown = Date.parse(endtimer) - Date.parse(new Date());
-    const seconds = Math.floor((countdown / 1000) % 60);
-    const minutes = Math.floor((countdown / 1000 / 60) % 60);
-    return { countdown, seconds, minutes };
-}
 
-const runTimer = (id, endtimer) => {
-    const timerPomo = document.getElementById(id);
-    const intervals = setInterval(() => {
-        const r_timer = timer(endtimer);
-        timerPomo.innerHTML = `${r_timer.minutes} : ${r_timer.seconds}`;
-        console.log(r_timer.minutes + r_timer.seconds);
-        if (r_timer.total <= 0) {
-            alert('Time to take a break');
-        }
-    }, 1000)
-}
